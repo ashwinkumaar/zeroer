d566aa9832d33ad720e41ee7c9f83093ea6bdb78
Qiya Tan <qiya.tan@u.nus.edu>
2024-06-27 17:24:45 +0800

    Add new user form

---
+++ ui-zeroer/src/AddUser.tsx
@@ -0,0 +1,71 @@
+"use client"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "./components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./components/ui/form"
+import { Input } from "./components/ui/input"
+
+
+// Get from API
+const formSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  addr: z.string(),
+  city: z.string(),
+  phone: z.string(),
+})
+
+export function AddUserForm() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        id: "",
+        name: "",
+        balance: "",
+        addr: "",
+        city: "",
+        phone: "",
+      },
+    })
+   
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+      // Do something with the form values.
+      // ✅ This will be type-safe and validated.
+      console.log(values)
+    }
+    //how to map zod object to form fields 
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        
+        {Object.keys(formSchema.shape).map((key:any) => (
+            <FormField
+                control={form.control}
+                name={key}
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>{key.charAt(0).toUpperCase() + key.slice(1)}</FormLabel>
+                        <FormControl>
+                            <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+        ))}
+        <Button type="submit">Add user</Button>
+      </form>
+    </Form>
+  )
+}
--- ui-zeroer/src/App.tsx
+++ ui-zeroer/src/App.tsx
@@ -1,16 +1,5 @@
+import { AddUserForm } from './AddUser';
 import DataSection from './components/sections/DataSection';
-import MainSection from './components/sections/MainSection';
-import { Button } from './components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from './components/ui/card';
-import { Input } from './components/ui/input';
-import { Label } from './components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
 
 function App() {
@@ -18,11 +7,11 @@ function App() {
     <div className='h-screen w-screen bg-white pt-12'>
       <Tabs defaultValue='main' className='mx-auto w-full max-w-screen-lg'>
         <TabsList className='grid w-full grid-cols-2'>
-          <TabsTrigger value='main'>Main</TabsTrigger>
+          <TabsTrigger value='main'>Add new user</TabsTrigger>
           <TabsTrigger value='data'>Data</TabsTrigger>
         </TabsList>
         <TabsContent value='main'>
-          <MainSection />
+          <AddUserForm />
         </TabsContent>
         <TabsContent value='data'>
           <DataSection />

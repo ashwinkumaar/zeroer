2e0108e1c93ae9473e5ca29b242d49b8ce7cb611
SeeuSim <seeusimong@gmail.com>
2024-06-28 10:39:00 +0800

    Add form fields for search
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- ui-zeroer/src/components/sections/data/TableFilter.tsx
+++ ui-zeroer/src/components/sections/data/TableFilter.tsx
@@ -1,12 +1,14 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { Table } from '@tanstack/react-table';
 import { FC } from 'react';
-
-import { DataType } from './columns';
-import { Button } from '@/components/ui/button';
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { useForm } from 'react-hook-form';
-import { TFilterSchema, filterDefaultValues, filterSchema } from './schema';
-import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { DataType } from './columns';
+import { TFilterSchema, TFilterSchemaKey, filterDefaultValues, filterSchema } from './schema';
+import { Input } from '@/components/ui/input';
 
 type TableFilterProps = {
   table: Table<DataType>;
@@ -29,22 +31,37 @@ export const TableFilter: FC<TableFilterProps> = ({ table: _table }) => {
       <div className='flex px-2'>
         <span className='text-sm font-medium'>Search for entities matching a user:</span>
       </div>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className='flex grow flex-row place-items-center justify-between gap-2 rounded-md border p-3 shadow-sm'
-      >
-        <div className='flex flex-row justify-evenly gap-4 text-sm'>
-          <div>Filter 1</div>
-          <div>Filter 2</div>
-          <div>Filter 3</div>
-          <div>Filter 4</div>
-          <div>Filter 5</div>
-        </div>
-        <Button className='flex flex-row gap-2 disabled:cursor-not-allowed' type='submit' disabled>
-          <MagnifyingGlassIcon />
-          <span>Search</span>
-        </Button>
-      </form>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex grow flex-row place-items-center justify-between gap-2 rounded-md border p-3 shadow-sm'
+        >
+          {Object.keys(filterSchema.shape).map((key) => (
+            <div>
+              <FormField
+                control={form.control}
+                key={key}
+                name={key as TFilterSchemaKey}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          ))}
+          <Button
+            className='flex flex-row gap-2 disabled:cursor-not-allowed'
+            type='submit'
+            disabled
+          >
+            <MagnifyingGlassIcon />
+            <span>Search</span>
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
--- ui-zeroer/src/components/sections/data/schema.ts
+++ ui-zeroer/src/components/sections/data/schema.ts
@@ -1,7 +1,9 @@
-import { TFormData, formDefaultValues, formSchema } from '@/components/sections/main';
+import { TFormData, TFormKey, formDefaultValues, formSchema } from '@/components/sections/main';
 
 export const filterSchema = formSchema;
 
 export type TFilterSchema = TFormData;
+
+export type TFilterSchemaKey = TFormKey;
 
 export const filterDefaultValues = formDefaultValues;
